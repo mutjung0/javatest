3e6ddd7b70eab2003f906caa9665b6b80acdc0d0
mutjung0 <mutjung0@gmail.com>
2024-11-06 14:47:53 +0900

    commit

--- src/string/StringSplit.java
+++ src/string/StringSplit.java
@@ -1,9 +1,17 @@
 package string;
 
+import java.util.Arrays;
+
 public class StringSplit {
 
     public static void main(String[] args) {
         String a = "abcd";
+        System.out.println(Arrays.toString(a.split(""))); // [a, b, c, d]
+
+        String b = "boo:and:foo";
+        System.out.println(Arrays.toString(b.split(":"))); // [boo, and, foo]
+        System.out.println(Arrays.toString(b.split("o"))); // [b, , :and:f]
+        // { "b", "", ":and:f"}
 
 
     }
